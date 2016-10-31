f40c0bbeb40b6eae40e51565a4364f7dbcd81777
Mtelnet <action_pan@mtelnet.com>
2016-10-31 16:36:17 +0800

    bottmSheetDialog

--- app/src/main/java/bottomdialog/demo/com/bottomdialogdemo/MainActivity.java
+++ app/src/main/java/bottomdialog/demo/com/bottomdialogdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package bottomdialog.demo.com.bottomdialogdemo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 普通的弹出bottomDialog
         dialog = (Button) findViewById(R.id.bottomdialog);
         sex = (TextView) findViewById(R.id.sex);
 
@@ -26,6 +31,61 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 selectSex();
+            }
+        });
+
+
+        // BottomSheetDialogFragment
+        final BottomSheetDialogFragment myBottomSheet = MyBottomSheetDialogFragment.newInstance("Modal Bottom Sheet");
+        Button fragment = (Button) findViewById(R.id.fragment);
+        fragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myBottomSheet.show(getSupportFragmentManager(), myBottomSheet.getTag());
+            }
+        });
+
+
+        // BottomSheetDialog
+        View bottomSheet = findViewById(R.id.design_bottom_sheet);
+        final BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_DRAGGING");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_SETTLING");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_EXPANDED");
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_COLLAPSED");
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_HIDDEN");
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Log.i("BottomSheetCallback", "slideOffset: " + slideOffset);
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (behavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
+                    behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
             }
         });
     }
---
+++ app/src/main/java/bottomdialog/demo/com/bottomdialogdemo/MyBottomSheetDialogFragment.java
@@ -0,0 +1,41 @@
+package bottomdialog.demo.com.bottomdialogdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by action on 16/10/31.
+ */
+
+public class MyBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    String mString;
+
+    static MyBottomSheetDialogFragment newInstance(String string) {
+        MyBottomSheetDialogFragment f = new MyBottomSheetDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("string", string);
+        f.setArguments(args);
+        return f;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mString = getArguments().getString("string");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.bottom_sheet_modal, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.text);
+        tv.setText(mString);
+        return v;
+    }
+}
